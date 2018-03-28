1c7b8a68ccf3e6cb090e229ada0ff66d7ef19b93
Unknown <yosifov@gmail.com>
2018-03-28 20:57:08 +0300

    Made some changes

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -5,14 +5,35 @@
 //  Created by Ivaylo Yosifov on 27/3/18.
 //  Copyright © 2018 Ivaylo Yosifov. All rights reserved.
 //
+// Hello - test for git
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var btnCount = 0;
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func btnTapped(_ sender: Any) {
+        
+        btnCount = btnCount + 1
+        if btnCount == 10 {
+            if (view.backgroundColor != UIColor.red) {
+                view.backgroundColor = UIColor.red
+                } else {
+                        view.backgroundColor = UIColor.white
+                        }
+            myLabel.text = "New Background Color"
+            btnCount = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
